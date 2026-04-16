bcfc259c3dfd4d0d436e862c3438cd8c755f53b1
jose <josealejadrohinojosa@gmail.com>
2026-04-16 13:34:04 -0500

    entrega final

--- src/main/java/com/superinka/ecosensor/backend/seguridad/SpringSecurityConfig.java
+++ src/main/java/com/superinka/ecosensor/backend/seguridad/SpringSecurityConfig.java
@@ -119,7 +119,10 @@ public class SpringSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200",
+        		"https://ecosensor-inka.netlify.app", 
+                "https://*.netlify.app"));
+        	
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // si usas cookies o auth
